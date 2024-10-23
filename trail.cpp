1dc29c51fcd8025ded95c4a369b7a6b1b62d5592
hclark37 <hclark37@vols.utk.edu>
2024-10-23 10:52:51 -0400

    Update trail.cpp

--- trail.cpp
+++ trail.cpp
@@ -1,3 +1,28 @@
 #include "trail.h"
 #include <iostream>
 
+using namespace std;
+
+trail::trail() : first(NULL) { 
+	
+	
+}
+
+void trail::addNode(const int &choice1[], const int &choice2[], const string &outputs[]) {
+	
+	
+}
+
+void trail::randomize() {
+	
+	
+}
+
+const string trail::checkStatus(int &stats[]) {
+	
+	
+}
+
+string Node::getData(void) const {
+	return string[0];	
+}
