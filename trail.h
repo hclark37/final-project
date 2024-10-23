bd201c78002ec0c2745196273eb2d56c49cdce80
hclark37 <hclark37@vols.utk.edu>
2024-10-23 08:30:51 -0400

    Create trail.h
    Template for the linked list implementation we'll be using

---
+++ trail.h
@@ -0,0 +1,34 @@
+#ifndef TRAIL_H
+#define TRAIL_H
+
+class Node {
+
+	friend class trail;
+
+	public:
+		Node(const int &);     
+		string getData() const;
+		string choice(bool, int[] &) const; //return strings[1 + (int)bool]
+	private:
+		string strings[3];
+		int choice1[4];
+		int choice2[4];
+		Node *next;
+};
+
+class trail {
+ public:
+	list();   
+	void randomize(); //store pointers in vector, shuffle vector, change pointers in nodes
+	string checkStatus(int[] &); //checks for death; checkStatus == "" means death
+	string win(int[] &);
+ private:
+	Node *newNode(const int &); 
+	Node *first;
+	Node *current;
+	string deathMessages[8];
+	string endings[4];
+};
+
+
+#endif
