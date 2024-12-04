28d0127f4c51c65c20f35f97688ff1e628a081b6
pdawes27 <pdawes@vols.utk.edu>
2024-12-03 19:20:33 -0500

    commenting

--- trail.h
+++ trail.h
@@ -11,13 +11,13 @@
 using namespace std;
 
 class Node {
-
 	friend class trail;
 
 	public:
 		Node(const int*, const int*, const string*);     
 		string getData() const; // gets event occurring
 		string choice(bool, int*); //changes stats and returns choice event
+	// Private variables are the events and choices at each node and next points to the next node
 	private:
 		string strings[3];
 		int choice1[4];
@@ -27,28 +27,28 @@ class Node {
 
 class trail {
  public:
-	trail(string);  
-	void addNode(const int*, const int*, const string*);
+	trail(string);  // Sets up the linked list and initial variables of the class
+	void addNode(const int*, const int*, const string*); // Adds nodes to the linked list
 	void randomize(); //store pointers in vector, shuffle vector, change pointers in nodes
 	string checkStatus(int*); //checks for death; checkStatus == "" means survival
-	string win(int*);
-	void Play(int*);
-	string choice(bool, int*);
-	void shop(int*);
-	void titleScreen(int*);
-	void Add(int*);
-	void settings(int*);
+	string win(int*); // If the players go through all the turns, then this happens
+	void Play(int*); // Main function to play the game
+	string choice(bool, int*); // This selects the stats used to add to total stats
+	void shop(int*); // A quick interjection to help the player out
+	void titleScreen(int*); // Main screen that goes to play, add evnets, settings and to quit
+	void Add(int*); // Add event to file
+	void settings(int*); // Can change turn total and shop frequency here
 	
  private:
 	Node *newNode(const int* choice1, const int* choice2, const string* outputs);
 	Node *first;
 	Node *current;
-	string fileName;
-	int turnCounter;
-	int totalTurns;
+	string fileName; // help pass the file
+	int turnCounter; // Count until it hits the turn total
+	int totalTurns; // total turns
 	int shopFrequency;
-	string deathMessages[8] = {"You became too poor and couldn't pay for basic survival needs.", "You became rich enough that you were robbed.", "You starved.", "You ate too much.", "You were too ill to carry on.", "Your body over stimulated itself.", "Being too paranoid, you drove yourself into insanity.", "Being too relaxed, you were jumped."};
-    string endings[4] = {"You came out better off after your journey!!", "You benefited from this journey!", "You struggled a little but complete your journey.", "You barely came out of this journey alive but survived."};
+	string deathMessages[8] = {"You became too poor and couldn't pay for basic survival needs.", "You became rich enough that you were robbed.", "You starved.", "You ate too much.", "You were too ill to carry on.", "Your body over stimulated itself.", "Being too paranoid, you drove yourself into insanity.", "Being too relaxed, you were jumped."}; // Death messages
+    	string endings[4] = {"You came out better off after your journey!!", "You benefited from this journey!", "You struggled a little but complete your journey.", "You barely came out of this journey alive but survived."}; // Winning endings
 };
 
 
